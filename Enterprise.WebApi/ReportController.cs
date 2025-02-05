86a6872b7a9c9c7a04bc590130fda4cbc8025b25
Engjell Voca <vo.en0312@gmail.com>
2025-02-05 21:25:09 +0100

    report controller

--- Enterprise.WebApi/ReportController.cs
+++ Enterprise.WebApi/ReportController.cs
@@ -1,6 +1,30 @@
-﻿namespace Enterprise.WebApi;
+﻿using AutoMapper;
+using Enterprise.Application.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Contracts.Dtos;
 
-public class ReportController
+namespace Enterprise.WebApi;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReportController : ControllerBase
 {
     
+    private readonly IReportService _reportService;
+    private readonly IMapper _mapper;
+    
+    public ReportController(IReportService reportService, IMapper mapper)
+    {
+        _reportService = reportService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet("supplier-delivery-times")]
+    public async Task<IActionResult> GetSupplierDeliveryTimes([FromQuery] Guid enterpriseId)
+    {
+            var deliveryTimes = await _reportService.GetSupplierDeliveryTimes(enterpriseId);
+            var deliveryTimesDto = _mapper.Map<List<SupplierDeliveryTimeDto>>(deliveryTimes);
+            return Ok(deliveryTimesDto);
+    }
 }
