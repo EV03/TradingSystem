9dac12baf9bf460353d70711c2510f5465128548
Engjell Voca <vo.en0312@gmail.com>
2025-02-05 15:21:12 +0100

    mapping dtos

---
+++ Shared.Contracts/Dtos/StockDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Contracts.Dtos;
+
+public class StockDto
+{
+    public Guid ProductId { get; set; }
+    public string Name { get; set; }
+    public int AvailableQuantity { get; set; }
+    public int IncomingQuantity { get; set; }
+}
